a4bbc75136def2a8de2e114a192c1fb6ed9f3723
nicolas-silva20 <101225917+nicolas-silva20@users.noreply.github.com>
2023-09-29 21:15:51 -0300

    Add files via upload

---
+++ Ejercicios Complementarios 28-9/Ejercicios Complementarios.py
@@ -0,0 +1,77 @@
+#Ejercicio 1
+numero1 = 20
+print(f"El numero 1 es: {numero1}")
+
+#Ejercicio 2
+numero2 = 5.5
+print(f"El numero 2 es: {numero2}")
+
+#Ejercicio 3
+suma = numero1 + numero2
+print(f"El resultado de la suma es: {suma}")
+
+#Ejercicio 4
+resta = numero1 + numero2
+multiplicacion = numero1 + numero2
+division = numero1 + numero2
+print(f"El resultado de la resta es: {resta}")
+print(f"El resultado de la multiplicación es: {multiplicacion}")
+print(f"El resultado de la división es: {division}")
+
+#Ejercicio 5
+nombre = "Nicolas"
+print(f"Mi nombre es: {nombre}")
+
+#Ejercicio 6
+precio = 12.5
+print(f"El precio es: {precio}")
+
+#Ejercicio 7
+descuento = 0.5
+print(f"El descuento es de {descuento}")
+
+#Ejercicio 8
+precio_final = precio * descuento
+print(f"El precio final es: {precio_final}")
+
+
+#Ejercicio 9
+cadena = "Buen día"
+
+#Ejercicio 10
+longitud = len(cadena)
+print(f"La cadena tiene una longitud de {longitud} caracteres")
+
+#Ejercicio 11
+precio = 10.5
+precio = int(precio)
+print(f"El nuevo valor del producto es: {precio}")
+
+
+#Ejercicio 12
+nombre = "Nicolas"
+apellido = "Silva"
+nombre_completo = nombre + " " + apellido
+print(f"Mi nombre completo es {nombre_completo}")
+
+#Ejercicio 13
+mi_edad = 22
+print(f"Mi edad es: {mi_edad} años")
+mi_edad = mi_edad + 5
+mi_edad = mi_edad - 10
+print(f"Mi edad luego del incremento y decremento es: {mi_edad}")
+
+#Ejercicio 14
+altura = 1.80
+print(f"Mi altura es: {altura}")
+altura = altura * 4
+altura = altura/3
+print(f"Mi altura luego de la multiplicación y división es {altura}")
+
+#Ejercicio 15
+nombre_mayus = "NICOLAS"
+nombre_minus = nombre_mayus.lower()
+print(f"Mi nombre en minúsculas es: {nombre_minus}")
+
+#Ejercicio 16
+print(f"Mi nombre con la primer letra en mayúscula es: {nombre_minus.capitalize()}")
